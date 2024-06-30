f6cfe4762aa394d43cf13319c414c7866b52d176
130e <fernival328@gmail.com>
2024-06-30 05:54:49 +0000

    Add settings to hyprland
    Doesn't know nixos hyprland has no default config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -33,6 +33,41 @@
   };
 
   wayland.windowManager.hyprland.enable = true;
+  wayland.windowManager.hyprland.settings = {
+    "$mod" = "SUPER";
+    bind =
+      [
+        "$mod, D, exec, firefox"
+        "$mod, Return, exec kitty"
+        "$mod, C, killactive"
+        "$mod, M, exit"
+        "$mod, V, togglefloating"
+
+        # Move focus with mainMod + arrow keys
+        "$mod, left, movefocus, l
+        "$mod, right, movefocus, r
+        "$mod, up, movefocus, u
+        "$mod, down, movefocus, d
+
+        ", Print, exec, grimblast copy area"
+      ]
+      ++ (
+        # workspaces
+        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+        builtins.concatLists (builtins.genList (
+            x: let
+              ws = let
+                c = (x + 1) / 10;
+              in
+                builtins.toString (x + 1 - (c * 10));
+            in [
+              "$mod, ${ws}, workspace, ${toString (x + 1)}"
+              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+            ]
+          )
+          10)
+      );
+  };
 
   programs.firefox.enable = true;
 
